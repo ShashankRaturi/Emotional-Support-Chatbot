2f461e4577dc4d3b637615ec8f730e468a3d187e
shashank <shashankraturi4@gmail.com>
2025-07-30 17:30:37 +0530

    replaced absolute paths with relative paths to avoid path conflict

--- Final Show/config.py
+++ Final Show/config.py
@@ -1,8 +1,22 @@
 import torch 
 
+from pathlib import Path
+## define base directory
+
+# Path(__file__) gets the path to the current file (config.py)
+# .parent moves one level up pointing to the parent directory
+BASE_DIR = Path(__file__).resolve().parent 
+
+## defining path of files dir
+FILES_DIR = BASE_DIR/"files"
+
+# define the paths
+VOCAB_PATH = FILES_DIR/"vocab.json"
+ENCODER_PATH = FILES_DIR/"label_encoder.pkl"
+MODEL_PATH = FILES_DIR/"emotion_rnn_checkpoint.pth"
+
+
+# other configurations
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-VOCAB_PATH = "/media/shashanks/3E0CD8C50CD878FB/CDAC/My work/CDAC PROJECTS/MACHINE LEARNING/Emotional-Support-Chatbot/Final Show/files/vocab.json"
-ENCODER_PATH = "/media/shashanks/3E0CD8C50CD878FB/CDAC/My work/CDAC PROJECTS/MACHINE LEARNING/Emotional-Support-Chatbot/Final Show/files/label_encoder.pkl"
-MODEL_PATH = "/media/shashanks/3E0CD8C50CD878FB/CDAC/My work/CDAC PROJECTS/MACHINE LEARNING/Emotional-Support-Chatbot/Final Show/files/emotion_rnn_checkpoint.pth"
 MAX_LEN = 500
 MIN_LEN = 31
